0f9170c485e147bf082baecfb2efa57e0fdc5055
nickdepaolo <79952881+nickdepaolo@users.noreply.github.com>
2021-05-09 23:10:04 -0400

    11:00 5/9

--- src/Components/NytResults.tsx
+++ src/Components/NytResults.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 
 type NytProps = {
-
+changePage: (event: any, direction: string) => any;
+results: any
 }
+
 
 
 export default class NytResults extends React.Component<NytProps, {}> {
@@ -11,14 +13,31 @@ export default class NytResults extends React.Component<NytProps, {}> {
     render() {
 
         return(
-
             <div>
-
-                <h1>555 timer</h1>
-
+                {this.props.results.map(result => {
+                    return(
+                        <div key={result.id}>
+                            <h3>{result.headline.main}</h3>
+                            {result.multimedia.length > 1 ? <img alt='article' src={`http://www.nytimes.com/${result.multimedia[1].url}`}/> : ''}
+                            <p>
+                                {result.snippet}
+                                <br/>
+                                {result.keywords.length > 0 ? 'Keywords:' : ''}
+                            </p>
+                                <ul>
+                                    {result.keywords.map(keyword => <li key={keyword.value}>{keyword.value}</li>)}
+                                </ul>
+                                <a href={result.web_url}><button>Read It</button></a>
+                                <div>
+                                    <button onClick={(e) => this.props.changePage(e, 'down')}>Previous 10</button>
+                                    <button onClick={(e) => this.props.changePage(e, 'up')}>Next 10</button>
+                                </div>
+                        </div>
+                    )
+                })}.
+                
             </div>
-            
-        )
+        );
 
     }
 
--- src/Components/NytSearch.tsx
+++ src/Components/NytSearch.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import NytResults from './NytResults'
 
 
-type NytProps = {
-
-}
-
 
 type NytState = {
 
@@ -16,13 +12,13 @@ type NytState = {
     results: Array<any>;
     url: string;
     key: string;
-
+   
 }
 
 
-export default class Nyt extends React.Component<NytProps, NytState> {
-    constructor(props: NytProps){
-        
+export default class Nyt extends React.Component<{}, NytState> {
+    constructor(props: {}){
+
         super(props)
 
         this.state = {
@@ -33,8 +29,42 @@ export default class Nyt extends React.Component<NytProps, NytState> {
             results: [],
             url: '',
             key: '',
+            
         }
 
+    }
+
+    fetchResults = () => {
+        let url = `${this.state.url}?api-key=${this.state.key}&page=${this.state.pageNumber}&q=${this.state.search}`;
+        url = this.state.startDate ? url + `&begin_date=${this.state.startDate}` : url;
+        url = this.state.endDate ? url + `end_date=${this.state.endDate}` : url;
+
+        fetch(url)
+        .then(res => res.json())
+        .then(data => this.setState({results: data.response.docs}))
+        .catch(err => console.log(err));
+    };
+
+    handleSubmit = (event: Event) => {
+        event.preventDefault();
+        this.setState({
+            pageNumber : 0
+        });
+        this.fetchResults();
+    }
+
+    changePageNumber = (event: Event, direction: string) => {
+        event.preventDefault()
+        if (direction === 'down') {
+            if (this.state.pageNumber > 0) {
+                this.setState({pageNumber: - 1});
+                this.fetchResults();
+            }
+        }
+        if (direction === 'up') {
+            this.setState({pageNumber: + 1});
+            this.fetchResults();
+        }
     }
 
 
@@ -42,16 +72,28 @@ export default class Nyt extends React.Component<NytProps, NytState> {
 
         return(
 
-            <div>
-
-                <h1>555 timer</h1>
-
-                <NytResults/>
-
+            <div className='main'>
+            <div className='mainDiv'>
+                <form onSubmit={(e) => this.handleSubmit(e) }>
+                    <span>Enter a single search term (required) :</span>
+                    <input type='text' name='search' onChange={(e) => this.setState({search: e.target.value})}/>
+                    <br/>
+                    <span>Enter a start date : </span>
+                    <input type='date' name='startDate' pattern='[0-9]{8}' onChange={(e) => this.setState({startDate: e.target.value})}/>
+                    <br/>
+                    <span>Enter a end date: </span>
+                    <input type='date' name='endDate' pattern='[0-9]{8}' onChange={(e) => this.setState({endDate: e.target.value})}/>
+                    <br/>
+                    <button className='submit'>Submit search</button>
+                </form>
+                {this.state.results.length > 0 ? <NytResults results={this.state.results} changePage={this.changePageNumber} /> : null}
             </div>
+        </div>
 
         )
 
     }
     
 }
+
+
